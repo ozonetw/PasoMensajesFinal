b467d30221bc2106e094d87eab9be027e1924384
rmelchorv <rmelchorv@gmail.com>
2015-09-11 15:50:34 -0500

    Final of final revision

---
+++ PasoMFilosofos/PasoMFilosofos/Main.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PasoMFilosofos
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Variable del diseñador requerida.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén utilizando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido del método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.estadoLemus = new System.Windows.Forms.PictureBox();
+			this.estadoPepe = new System.Windows.Forms.PictureBox();
+			this.estadoHugo = new System.Windows.Forms.PictureBox();
+			this.estadoArturo = new System.Windows.Forms.PictureBox();
+			this.estadoViejo = new System.Windows.Forms.PictureBox();
+			this.hamburguesa = new System.Windows.Forms.PictureBox();
+			this.hugo = new System.Windows.Forms.PictureBox();
+			this.pepe = new System.Windows.Forms.PictureBox();
+			this.lemus = new System.Windows.Forms.PictureBox();
+			this.arturo = new System.Windows.Forms.PictureBox();
+			this.viejo = new System.Windows.Forms.PictureBox();
+			this.consola = new System.Windows.Forms.ListBox();
+			((System.ComponentModel.ISupportInitialize)(this.estadoLemus)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoPepe)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoHugo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoArturo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoViejo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.hamburguesa)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.hugo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pepe)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.lemus)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.arturo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.viejo)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// estadoLemus
+			// 
+			this.estadoLemus.Location = new System.Drawing.Point(8, 180);
+			this.estadoLemus.Name = "estadoLemus";
+			this.estadoLemus.Size = new System.Drawing.Size(62, 50);
+			this.estadoLemus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.estadoLemus.TabIndex = 33;
+			this.estadoLemus.TabStop = false;
+			// 
+			// estadoPepe
+			// 
+			this.estadoPepe.Location = new System.Drawing.Point(240, 342);
+			this.estadoPepe.Name = "estadoPepe";
+			this.estadoPepe.Size = new System.Drawing.Size(62, 50);
+			this.estadoPepe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.estadoPepe.TabIndex = 32;
+			this.estadoPepe.TabStop = false;
+			// 
+			// estadoHugo
+			// 
+			this.estadoHugo.Location = new System.Drawing.Point(455, 354);
+			this.estadoHugo.Name = "estadoHugo";
+			this.estadoHugo.Size = new System.Drawing.Size(62, 50);
+			this.estadoHugo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.estadoHugo.TabIndex = 31;
+			this.estadoHugo.TabStop = false;
+			// 
+			// estadoArturo
+			// 
+			this.estadoArturo.Location = new System.Drawing.Point(528, 180);
+			this.estadoArturo.Name = "estadoArturo";
+			this.estadoArturo.Size = new System.Drawing.Size(62, 50);
+			this.estadoArturo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.estadoArturo.TabIndex = 30;
+			this.estadoArturo.TabStop = false;
+			// 
+			// estadoViejo
+			// 
+			this.estadoViejo.Location = new System.Drawing.Point(350, 62);
+			this.estadoViejo.Name = "estadoViejo";
+			this.estadoViejo.Size = new System.Drawing.Size(62, 50);
+			this.estadoViejo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.estadoViejo.TabIndex = 29;
+			this.estadoViejo.TabStop = false;
+			// 
+			// hamburguesa
+			// 
+			this.hamburguesa.BackgroundImage = global::PasoMFilosofos.Properties.Resources.hamburguesa;
+			this.hamburguesa.Image = global::PasoMFilosofos.Properties.Resources.hamburguesa;
+			this.hamburguesa.Location = new System.Drawing.Point(261, 174);
+			this.hamburguesa.Name = "hamburguesa";
+			this.hamburguesa.Size = new System.Drawing.Size(82, 77);
+			this.hamburguesa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.hamburguesa.TabIndex = 28;
+			this.hamburguesa.TabStop = false;
+			// 
+			// hugo
+			// 
+			this.hugo.Image = global::PasoMFilosofos.Properties.Resources.mitre;
+			this.hugo.Location = new System.Drawing.Point(346, 304);
+			this.hugo.Name = "hugo";
+			this.hugo.Size = new System.Drawing.Size(103, 101);
+			this.hugo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.hugo.TabIndex = 27;
+			this.hugo.TabStop = false;
+			// 
+			// pepe
+			// 
+			this.pepe.Image = global::PasoMFilosofos.Properties.Resources.pepe;
+			this.pepe.Location = new System.Drawing.Point(148, 304);
+			this.pepe.Name = "pepe";
+			this.pepe.Size = new System.Drawing.Size(89, 89);
+			this.pepe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pepe.TabIndex = 26;
+			this.pepe.TabStop = false;
+			// 
+			// lemus
+			// 
+			this.lemus.Image = global::PasoMFilosofos.Properties.Resources.lemus;
+			this.lemus.Location = new System.Drawing.Point(71, 130);
+			this.lemus.Name = "lemus";
+			this.lemus.Size = new System.Drawing.Size(103, 101);
+			this.lemus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.lemus.TabIndex = 25;
+			this.lemus.TabStop = false;
+			// 
+			// arturo
+			// 
+			this.arturo.Image = global::PasoMFilosofos.Properties.Resources.arturo;
+			this.arturo.Location = new System.Drawing.Point(423, 130);
+			this.arturo.Name = "arturo";
+			this.arturo.Size = new System.Drawing.Size(103, 101);
+			this.arturo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.arturo.TabIndex = 24;
+			this.arturo.TabStop = false;
+			// 
+			// viejo
+			// 
+			this.viejo.Image = global::PasoMFilosofos.Properties.Resources.viejo;
+			this.viejo.Location = new System.Drawing.Point(240, 12);
+			this.viejo.Name = "viejo";
+			this.viejo.Size = new System.Drawing.Size(103, 101);
+			this.viejo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.viejo.TabIndex = 23;
+			this.viejo.TabStop = false;
+			// 
+			// consola
+			// 
+			this.consola.FormattingEnabled = true;
+			this.consola.Location = new System.Drawing.Point(3, 442);
+			this.consola.Name = "consola";
+			this.consola.Size = new System.Drawing.Size(587, 160);
+			this.consola.TabIndex = 34;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(592, 604);
+			this.Controls.Add(this.consola);
+			this.Controls.Add(this.estadoLemus);
+			this.Controls.Add(this.estadoPepe);
+			this.Controls.Add(this.estadoHugo);
+			this.Controls.Add(this.estadoArturo);
+			this.Controls.Add(this.estadoViejo);
+			this.Controls.Add(this.hamburguesa);
+			this.Controls.Add(this.hugo);
+			this.Controls.Add(this.pepe);
+			this.Controls.Add(this.lemus);
+			this.Controls.Add(this.arturo);
+			this.Controls.Add(this.viejo);
+			this.Icon = global::PasoMFilosofos.Properties.Resources.fork;
+			this.Name = "Main";
+			this.Text = "Filosofos comensales";
+			this.Load += new System.EventHandler(this.Main_Load);
+			((System.ComponentModel.ISupportInitialize)(this.estadoLemus)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoPepe)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoHugo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoArturo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.estadoViejo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.hamburguesa)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.hugo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pepe)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.lemus)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.arturo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.viejo)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox estadoLemus;
+		private System.Windows.Forms.PictureBox estadoPepe;
+		private System.Windows.Forms.PictureBox estadoHugo;
+		private System.Windows.Forms.PictureBox estadoArturo;
+		private System.Windows.Forms.PictureBox estadoViejo;
+		private System.Windows.Forms.PictureBox hamburguesa;
+		private System.Windows.Forms.PictureBox hugo;
+		private System.Windows.Forms.PictureBox pepe;
+		private System.Windows.Forms.PictureBox lemus;
+		private System.Windows.Forms.PictureBox arturo;
+		private System.Windows.Forms.PictureBox viejo;
+		private System.Windows.Forms.ListBox consola;
+	}
+}
+
---
+++ PasoMFilosofos/PasoMFilosofos/Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PasoMFilosofos
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            Form.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //Una sóla instancia del recurso compartido
+			//	que se comparte entre los procesos
+            Tenedores tenedores = new Tenedores();
+
+            //Se crean las instancias de los filosofos
+            Filosofo filosofo0 = new Filosofo(0, tenedores, evento_CambioEstado);
+            Filosofo filosofo1 = new Filosofo(1, tenedores, evento_CambioEstado);
+            Filosofo filosofo2 = new Filosofo(2, tenedores, evento_CambioEstado);
+            Filosofo filosofo3 = new Filosofo(3, tenedores, evento_CambioEstado);
+            Filosofo filosofo4 = new Filosofo(4, tenedores, evento_CambioEstado);
+        }
+
+		/// <summary>
+		/// Manejador de evento para cambio de estado de un proceso
+		/// </summary>
+		/// <param name="sender">Objeto que identifica al proceso que lanza el evento para notificar su cambio de estado</param>
+		/// <param name="e"></param>
+        private void evento_CambioEstado(object sender, EventArgs e)
+        {
+            Argumentos proceso = (Argumentos)sender;
+
+          	//Escribe en la consola para identificar al proceso y su estado
+            consola.Items.Add("Filosofo " + proceso.IdProceso + ": " + proceso.Estado);
+			//Actualiza el gráfico del estado del proceso
+			EstablecerEstado(proceso);
+        }
+
+		/// <summary>
+		/// Actualiza el gráfico de estado de un proceso: Pensando (VERDE), Esperando (AMARILLO), Comiendo (ROJO)
+		/// </summary>
+		/// <param name="proceso">Clase que identifica al proceso que lanza el evento para notificar su cambio de estado</param>
+		private void EstablecerEstado(Argumentos proceso)
+		{
+			switch(proceso.IdProceso)
+			{
+				case 0:
+					estadoViejo.Image = (Image)global::PasoMFilosofos.Properties.Resources.ResourceManager.GetObject(proceso.Estado.ToString());
+					break;
+				case 1:
+					estadoLemus.Image = (Image)global::PasoMFilosofos.Properties.Resources.ResourceManager.GetObject(proceso.Estado.ToString());
+					break;
+				case 2:
+					estadoPepe.Image = (Image)global::PasoMFilosofos.Properties.Resources.ResourceManager.GetObject(proceso.Estado.ToString());
+					break;
+				case 3:
+					estadoHugo.Image = (Image)global::PasoMFilosofos.Properties.Resources.ResourceManager.GetObject(proceso.Estado.ToString());
+					break;
+				case 4:
+					estadoArturo.Image = (Image)global::PasoMFilosofos.Properties.Resources.ResourceManager.GetObject(proceso.Estado.ToString());
+					break;
+				default:
+					break;
+			}
+		}
+    }
+}
